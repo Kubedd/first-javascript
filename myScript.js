95f74db9ef97354616557e5fb79a462a6b80039d
Kubedd <45365899+Kubedd@users.noreply.github.com>
2018-12-18 08:23:24 -0700

    added Math Function

--- myScript.js
+++ myScript.js
@@ -1,3 +1,15 @@
+// This function changes the inner html of the id of Demo
 function myfunction () {
     document.getElementById('demo').innerHTML = "Paragraph changed";
+}
+
+//Defining Variables
+
+var x, y, z;
+x = 10;
+y = 10;
+z = x + y;
+
+function math() {
+    document.getElementById('math').innerHTML = z;
 }
