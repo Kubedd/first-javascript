6a1210249e0a75c2b458446610daf5f48b90dfd2
Kubedd <45365899+Kubedd@users.noreply.github.com>
2019-01-07 10:22:17 -0700

    We Real Math Now

--- myScript.js
+++ myScript.js
@@ -3,13 +3,9 @@ function myfunction () {
     document.getElementById('demo').innerHTML = "Paragraph changed";
 }
 
-//Defining Variables
 
-var x, y, z;
-x = 10;
-y = 10;
-z = x + y;
 
-function math() {
-    document.getElementById('math').innerHTML = z;
+
+function math(p1, p2) {
+    return p1 + p2;
 }
